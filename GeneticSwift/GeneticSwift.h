45544fad95915915f72a8e36db17787b1b8f8174
Jose Maria <josemaria.puerta@bq.com>
2017-01-21 02:03:23 +0100

    Added project

---
+++ GeneticSwift/GeneticSwift.h
@@ -0,0 +1,19 @@
+//
+//  GeneticSwift.h
+//  GeneticSwift
+//
+//  Created by Jose Maria Puerta on 21/1/17.
+//  Copyright © 2017 Jose Maria Puerta. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GeneticSwift.
+FOUNDATION_EXPORT double GeneticSwiftVersionNumber;
+
+//! Project version string for GeneticSwift.
+FOUNDATION_EXPORT const unsigned char GeneticSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeneticSwift/PublicHeader.h>
+
+
